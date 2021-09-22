e6ba9324ed3c3b70d0195dd1e2727c9e0a4d55e8
PavelSuchkov <suchkovp@gmail.com>
2021-09-22 13:58:54 +0300

    refactoring

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -16,8 +16,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
     }
 ) => {
     const mappedOptions: any[] = options
-        ? options.map((o, i) => (
-            <option className={s.option}  key={o + '-' + i} value={o}>{o}</option>))
+        ? options.map((o, i) => (<option className={s.option}  key={o + '-' + i} value={o}>{o}</option>))
         : []// map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
